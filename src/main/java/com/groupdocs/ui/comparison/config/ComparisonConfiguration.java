e30c06a666c55fdc13af349f97e0b95155bb7380
Elena Kolesnikova <koleslena@yandex.ru>
2018-11-21 12:36:40 +0200

    added default folders for license and files, added small fixes

--- src/main/java/com/groupdocs/ui/comparison/config/ComparisonConfiguration.java
+++ src/main/java/com/groupdocs/ui/comparison/config/ComparisonConfiguration.java
@@ -1,11 +1,15 @@
 package com.groupdocs.ui.comparison.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.groupdocs.ui.common.config.CommonConfiguration;
+import io.dropwizard.Configuration;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.Valid;
 
-public class ComparisonConfiguration extends CommonConfiguration {
+import static com.groupdocs.ui.common.config.DefaultDirectories.defaultComparisonDirectory;
+import static com.groupdocs.ui.common.config.DefaultDirectories.relativePathToAbsolute;
+
+public class ComparisonConfiguration extends Configuration {
 
     @Valid
     @JsonProperty
@@ -28,7 +32,7 @@ public class ComparisonConfiguration extends CommonConfiguration {
     }
 
     public void setFilesDirectory(String filesDirectory) {
-        this.filesDirectory = filesDirectory;
+        this.filesDirectory = StringUtils.isEmpty(filesDirectory) ? defaultComparisonDirectory() : relativePathToAbsolute(filesDirectory);
     }
 
     public String getResultDirectory() {
